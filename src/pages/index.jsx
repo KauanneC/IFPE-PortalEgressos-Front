238393b8cc96416b08c4749a72379a86e709327c
Kauanne Cavalcante <kca1@discente.ifpe.edu.br>
2023-10-31 17:36:13 -0300

    :tada: Feat: Projeto Criado

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useState } from "react";
+
+// Page Principal Home
+export default function home() {
+
+    return (
+        <section id='conteudo' className="ml-65 mr-65 font-cabin">
+            <div className='text-center py-8'>
+                <h1 className="text-2xl font-semibold text-azulBase">Abulele</h1>
+            </div>
+
+        </section>
+    )
+}
