91985d7592b1c855071c91af558ab743568b891a
Kauanne Cavalcante <kca1@discente.ifpe.edu.br>
2023-11-01 08:50:54 -0300

    Atualizações

---
+++ src/pages/adm/formAdministrator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function FormAdministrator() {
+    return (
+        <main className="bg-cinza10 font-cabin">
+            <header></header>
+            <section>
+                <h1>Formulário</h1>
+            </section>
+        </main>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,59 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'azulEscuro': '#131736',
+        'azulBase': '#242B63',
+        'azulLink': '#142797',
+        'azulForm': '#87A8CD',
+        'fundoEvento': '#9EB8D6',
+        'vermelhoButton': '#991D39',
+        'verdeButton': '#20771B',
+        'pretoTexto': '323344',
+        'cinza01': '#191919',
+        'cinza02': '#333333',
+        'cinza03': '#4D4D4D',
+        'cinza04': '#555555',
+        'cinza05': '#666666',
+        'cinza06': '#808080',
+        'cinza07': '#999999',
+        'cinza08': '#B3B3B3',
+        'cinza09': '#CCCCCC',
+        'cinza10': '#E6E6E6',
+        'preto': '#000000',
+        'branco': '#FFFFFF',
+      },
+
+      fontFamily: {
+        'cabin': ['Cabin', 'sans-serif'],
+      },
+
+      fontSize: {
+        'tituloDestaque': '55px',
+        'textoDestaque': '44px',
+        'tituloPrincial': '35px',
+        'tituloSessão': '28px',
+        'subtitulo': '23px',
+        'paragrafo': '18px',
+        'legenda': '14px',
+      },
+
+      margin: {
+        '10': '10px',
+        '15': '15px',
+        '20': '20px',
+        '25': '25px',
+        '30': '30px',
+        '120': '120px',
+      },
+
+      padding: {
+        '10': '10px',
+        '15': '15px',
+        '20': '20px',
+        '25': '25px',
+        '30': '30px',
       },
     },
   },
-  plugins: [],
 }
