df4a5d3bdd37e114431c0442b8e8cc376238213d
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2023-11-02 20:35:48 -0300

    feat (login) + update (navbar user)

--- src/components/navBar/user/index.jsx
+++ src/components/navBar/user/index.jsx
@@ -9,12 +9,12 @@ function NavBar() {
         <div id='navmenu' className="bg-fundo px-30 py-4 justify-between flex">
             <div className="space-x-5 w-min">
                 <Link href="/" className={`text-azulBase ${router.pathname === '/' ? 'border-b-2 border-azulBase' : ''}`}>Home</Link>
-                <Link href="/pages/user/eventos" className={`text-azulBase ${router.pathname === '/pages/user/eventos' ? 'border-b-2 border-azulBase' : ''}`}>Eventos</Link>
-                <Link href="/pages/user/editais" className={`text-azulBase ${router.pathname === '/pages/user/editais' ? 'border-b-2 border-azulBase' : ''}`}>Editais</Link>
+                <Link href="/user/eventos" className={`text-azulBase ${router.pathname === '/pages/user/eventos' ? 'border-b-2 border-azulBase' : ''}`}>Eventos</Link>
+                <Link href="/user/editais" className={`text-azulBase ${router.pathname === '/user/editais' ? 'border-b-2 border-azulBase' : ''}`}>Editais</Link>
             </div>
             <div className='w-min'>
                 <button className='bg-azulBase pb-1 py-0.5 px-15 text-cinza10 rounded-lg'>
-                    Entrar
+                    <Link href="/user/login">Entrar</Link>
                 </button>
             </div>
         </div>
