e071984e31d146f7293c814987ed6f62b9208055
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2023-11-06 17:45:51 -0300

    feat (forms egresso) alert voltar

--- src/pages/egresso/formStep04/index.jsx
+++ src/pages/egresso/formStep04/index.jsx
@@ -8,6 +8,7 @@ import NavAcessibilidade from '@/components/navAcessibilidade';
 import NavBar from '@/components/navBar/egresso';
 import Footer from '@/components/footer';
 
+import iconPerigo from '/public/icons/iconPerigo.svg';
 import iconVoltar from '/public/icons/iconVoltar.svg';
 import iconDadosPessoaisBlue from '/public/icons/iconDadosPessoaisBlue.svg';
 import iconAcademicoBlue from '/public/icons/iconAcademicoBlue.svg';
@@ -19,6 +20,7 @@ import iconSucesso from '/public/icons/iconSucesso.svg';
 export default function FormStep04() {
 
     const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [isPopupBackOpen, setIsPopupBackOpen] = useState(false);
 
     const openPopup = () => {
         setIsPopupOpen(true);
@@ -33,6 +35,19 @@ export default function FormStep04() {
         closePopup();
     };
 
+    const openPopupBack = () => {
+        setIsPopupBackOpen(true);
+    };
+
+    const closePopupBack = () => {
+        setIsPopupBackOpen(false);
+    };
+
+    const handleConfirmBack = () => {
+        router.push('/egresso/home');
+        closePopupBack();
+    };    
+
     return (
         <main className="bg-cinza10">
             <header>
@@ -42,9 +57,9 @@ export default function FormStep04() {
             <section id="conteudo" className='mt-30 space-y-15 mx-120 items-center justify-center flex flex-col relative'>
                 {/* Título Form */}
                 <div className="bg-fundo w-4/5 py-30 px-60 rounded-lg border-t-8 border-azulForm">
-                    <Link href='/egresso/home' className="absolute top-0 left-0">
+                    <button onClick={openPopupBack} className="absolute top-0 left-0">
                         <Image src={iconVoltar} alt="Voltar para página inicial" />
-                    </Link>
+                    </button>
                     <h1 className='font-semibold text-azulBase text-tituloSessão text-center'>Acompanhamento de Egressos do Curso de Engenharia de Software - IFPE Campus Belo Jardim</h1>
                 </div>
                 {/* Navegação Form */}
@@ -109,6 +124,7 @@ export default function FormStep04() {
                     </div>
                 </div>
             </section>
+
             {isPopupOpen && (
                 <div className="fixed inset-0 flex items-center justify-center z-50">
                     <div className="absolute w-full h-full bg-black opacity-50"></div>
@@ -124,6 +140,24 @@ export default function FormStep04() {
                     </div>
                 </div>
             )}
+
+            {isPopupBackOpen && (
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                    <div className="absolute w-full h-full bg-black opacity-50"></div>
+                    <div className="relative bg-white p-4 rounded-lg shadow-lg">
+                        <div className='flex flex-col items-center justify-center space-y-15 mx-30 my-15'>
+                            <Image src={iconPerigo} alt="Perigo" />
+                            <h1 className='text-tituloSessão text-azulBase font-semibold'>Tem certeza?</h1>
+                            <p className='text-paragrafo text-pretoTexto'>Os dados não serão salvos se voltar para a página inicial</p>
+                            <div className="space-x-15">
+                                <button className="px-15 py-5 bg-azulBase font-semibold text-cinza10 rounded-lg hover:bg-azulForm hover:text-pretoTexto" onClick={handleConfirmBack}>Sim</button>
+                                <button className="px-15 py-5 bg-azulBase font-semibold text-cinza10 rounded-lg hover:bg-azulForm hover:text-pretoTexto" onClick={closePopupBack}>Não</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
             <footer>
                 <Footer />
             </footer>
