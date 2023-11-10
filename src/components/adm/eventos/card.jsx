6e3ebf0bb81b57a26af28cc22e866f19b6ebe503
Kauanne Cavalcante <kca1@discente.ifpe.edu.br>
2023-11-10 16:22:06 -0300

    Feat: Tela Eventos Concluída

--- src/components/adm/eventos/card.jsx
+++ src/components/adm/eventos/card.jsx
@@ -1,18 +1,59 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 
 export default function Card() {
 
-    const dados = [{
-        titulo: "Titulo",
-        data: "Data",
-        modalidade: "Modalidade",
-        local: "Local",
-        descricao: "Descrição",
-    }]
-    
+    const [expanded, setExpanded] = useState(false);
+
+    const descricao = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus libero sem, fringilla eget nisi tempus, vehicula tempor odio. Sed vel metus fringilla purus facilisis pharetra sed sit amet ipsum. Morbi eget ornare risus, accumsan consectetur enim. Nunc porta augue leo, at vehicula tortor hendrerit sit amet. Cras vitae ultricies massa.`;
+
+    // const descricao = expanded ? descricaoCompleta : descricaoCompleta.slice(0, 100); // Exibe os primeiros 100 caracteres ou o texto completo, com base no estado "expanded"
+
+    const handleToggleExpand = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <div className="w-310 h-341 bg-white rounded-10 border-t-10 border-azulForm drop-shadow">
-            <h1>{dados.titulo}</h1>
+        <div className={`w-400 bg-white rounded-10 border-t-10 border-azulForm drop-shadow border ${expanded ? 'max-h-auto' : ''}`}>
+            <h2 className="text-pretoTexto font-bold text-tituloSessão text-center mt-30">Workshop de UI/UX</h2>
+            <div className="flex flex-col mt-15 mx-20 gap-10">
+                <div className="flex flex-row gap-5">
+                    <p className="font-semibold text-azulBase">Data:</p>
+                    <p className="text-pretoTexto text-paragrafo">05/12/2023 (Terça-Feira)</p>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <p className="font-semibold text-azulBase">Horário:</p>
+                    <p className="text-pretoTexto text-paragrafo">13h</p>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <p className="font-semibold text-azulBase">Modalidade:</p>
+                    <p className="text-pretoTexto text-paragrafo">Online</p>
+                </div>
+                <div className="flex flex-row gap-5">
+                    <p className="font-semibold text-azulBase">Local:</p>
+                    <p className="text-pretoTexto text-paragrafo">https://meet.google.com/gab-gqjc-zmp</p>
+                </div>
+                <div className="break-words flex flex-row gap-5 mb-30">
+                    <p className="font-semibold text-azulBase">Descrição:</p>
+                    <div className="text-pretoTexto text-paragrafo break-words">
+                        {descricao.length > 90 ? (
+                            <div>
+                                <p className="leading-snug">
+                                    {expanded ? descricao : `${descricao.slice(0, 60)}...`}
+                                </p>
+                                <button
+                                    className='text-azulBase text-paragrafo font-medium'
+                                    onClick={handleToggleExpand}
+                                >
+                                    {expanded ? 'Ver menos' : 'Ver mais'}
+                                </button>
+                            </div>
+                        ) : (
+                            <p>{descricao}</p>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/navBar/adm/page.jsx
+++ src/components/navBar/adm/page.jsx
@@ -12,10 +12,10 @@ function NavBar() {
         <div id='navmenu' className="bg-fundo py-4 justify-between flex">
             <div className="w-ful">
                 <Link href="/home" className={`text-azulBase ml-30 mr-10 ${router.pathname === '/' ? 'border-b-2 border-azulBase' : ''}`}>Home</Link>
-                <Link href="/adm/eventos/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/eventos/page' ? 'border-b-2 border-azulBase' : ''}`}>Eventos</Link>
-                <Link href="/adm/editais/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/editais/page' ? 'border-b-2 border-azulBase' : ''}`}>Editais</Link>
-                <Link href="/adm/egressos/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/egressospage' ? 'border-b-2 border-azulBase' : ''}`}>Egressos</Link>
-                <Link href="/adm/form/formAdministrator" className={`text-azulBase ${router.pathname === '/adm/form/formAdministrator' ? 'border-b-2 border-azulBase' : ''}`}>Formulário</Link>
+                <Link href="/adm/eventos/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/eventos/page' ? 'p-5 border-b-2 border-azulBase' : ''}`}>Eventos</Link>
+                <Link href="/adm/editais/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/editais/page' ? 'p-5 border-b-2 border-azulBase' : ''}`}>Editais</Link>
+                <Link href="/adm/egressos/page" className={`text-azulBase mr-10 ${router.pathname === '/adm/egressospage' ? 'p-5 border-b-2 border-azulBase' : ''}`}>Egressos</Link>
+                <Link href="/adm/form/page" className={`text-azulBase ${router.pathname === '/adm/form/page' ? 'p-5 border-b-2 border-azulBase' : ''}`}>Formulário</Link>
             </div>
             <div className='justify-center flex mr-30'>
                 <button>
