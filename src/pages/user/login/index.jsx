6ba9044e79eecf4baa95b6ce3e6c44b77cec5fb5
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2023-11-26 17:00:51 -0300

    feat (login) concluído testes + jwt

--- src/pages/user/login/index.jsx
+++ src/pages/user/login/index.jsx
@@ -1,7 +1,10 @@
+"use client"
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import jwt from 'jsonwebtoken';
 
 import IconVoltar from "/public/icons/iconVoltar.svg";
 import ImgLogin from "/public/icons/imgLogin.svg";
@@ -10,18 +13,116 @@ import IconSenha from "/public/icons/iconSenha.svg";
 import IconSee from "/public/icons/iconSee.svg";
 import IconUnsee from "/public/icons/iconUnsee.svg";
 
+// API
+import { authenticate } from "../../../../utils/apiLogin/api";
+
 export default function Login() {
 
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
-        console.log(showPassword)
-    }
+    };
 
     const handleBack = () => {
         window.history.back();
+    };
+
+    const descriptografarToken = (token) => {
+        try {
+            const decoded = jwt.decode(token);
+            return decoded;
+        } catch (error) {
+            console.error('Erro ao descriptografar o token:', error.message);
+            return null;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const data = {
+                email: email,
+                password: password,
+            };
+            const response = await authenticate(data);
+            if (response && response.statusCode === 200) {
+                const token = response.data.token;
+                const decoded = descriptografarToken(token);
+                console.log('decoded:', decoded);
+                Swal.fire({
+                    icon: "success",
+                    iconColor: '#20771B',
+                    title: "Sucesso!",
+                    text: "Login realizado com sucesso!",
+                    confirmButtonText: "Ok",
+                    confirmButtonColor: "#242B63",
+                    showCancelButton: false,
+                    showConfirmButton: true,
+                    allowOutsideClick: false, // Impede que o usuário feche o alert clicando fora dele
+                    allowEscapeKey: false, // Impede que o usuário feche o alert usando a tecla ESC
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        if (decoded) {
+                            console.log('ID do usuário:', decoded.userId);
+                            console.log('Perfil do usuário:', decoded.profile);
+                            if (decoded.profile === 'coordinator') {
+                                if (decoded.primaryAcess == false) {
+                                    window.location.href = '/egresso/primeiroAcesso';
+                                    decoded.primaryAcess = true;
+                                } else {
+                                    window.location.href = '/egresso/home';
+                                }
+                            } else if (decoded.profile === 'student') {
+                                if (decoded.primaryAcess == true) {
+                                    window.location.href = '/egresso/primeiroAcesso';
+                                } else {
+                                    window.location.href = '/egresso/home';
+                                    decoded.primaryAcess = true;
+                                }
+                            } else if (decoded.profile === 'teacher') {
+                                if (decoded.primaryAcess == true) {
+                                    window.location.href = '/docente/primeiroAcesso';
+                                } else {
+                                    window.location.href = '/docente/home';
+                                    decoded.primaryAcess = true;
+                                }
+                            }
+                        }
+                    }
+                });
+            } else {
+                console.error('Erro na autenticação:', response);
+                Swal.fire({
+                    icon: "error",
+                    iconColor: '#991D39',
+                    title: "Ops...",
+                    text: "Email e/ou senha incorretos!",
+                    confirmButtonText: "Tentar novamente",
+                    confirmButtonColor: "#242B63",
+                });
+            }
+        } catch (error) {
+            console.error('Erro ao fazer requisição:', error.message);
+            Swal.fire({
+                icon: "error",
+                iconColor: '#991D39',
+                title: "Ops...",
+                text: "Erro ao fazer requisição!",
+                confirmButtonText: "Tentar novamente",
+                confirmButtonColor: "#242B63",
+            });
+        }
     };
 
     return (
@@ -30,9 +131,9 @@ export default function Login() {
                 <div className="w-4/6">
                     <Image src={ImgLogin} className="object-cover h-screen w-full" alt="Imagem de alunos se formando" />
                 </div>
-                <div className="w-3/6 flex flex-col justify-center px-60">
+                <form onSubmit={handleSubmit} className="w-3/6 flex flex-col justify-center px-60">
                     <button onClick={handleBack}>
-                        <Image src={IconVoltar} alt="Voltar" className="mt-30"/>
+                        <Image src={IconVoltar} alt="Voltar" className="mb-30" />
                     </button>
                     <h1 className="font-semibold text-azulBase text-tituloDestaque mb-100">Login</h1>
                     <div className="space-y-30">
@@ -44,6 +145,8 @@ export default function Login() {
                                 className="w-full px-2 h-10 pl-9 text-pretoTexto border-b-2 border-cinza07 focus:outline-none text-input required"
                                 type="email"
                                 placeholder="Email ou SIAPE"
+                                value={email}
+                                onChange={handleEmailChange}
                             />
                         </div>
                         <div className="w-full relative text-paragrafo">
@@ -54,6 +157,8 @@ export default function Login() {
                                 className="w-full px-2 h-10 pl-9 text-pretoTexto border-b-2 border-cinza07 focus:outline-none text-input required"
                                 type={showPassword ? 'text' : 'password'}
                                 placeholder="Senha"
+                                value={password}
+                                onChange={handlePasswordChange}
                             />
                             <div className="absolute inset-y-0 right-0 flex items-center pr-10 cursor-pointer" onClick={togglePasswordVisibility}>
                                 {showPassword ? (
@@ -67,12 +172,12 @@ export default function Login() {
                             </p>
                         </div>
                         <div>
-                            <button className="bg-azulBase mt-10 py-10 text-cinza10 text-sub font-semibold rounded-lg w-full transition-transform transform hover:scale-105 active:bg-azulEscuro">
-                                <Link href={'../../egresso/primeiroAcesso'}>Entrar</Link>
+                            <button type="submit" className="bg-azulBase mt-10 py-10 text-cinza10 text-sub font-semibold rounded-lg w-full transition-transform transform hover:scale-105 active:bg-azulEscuro">
+                                Entrar
                             </button>
                         </div>
                     </div>
-                </div>
+                </form>
             </section>
         </main>
     )
