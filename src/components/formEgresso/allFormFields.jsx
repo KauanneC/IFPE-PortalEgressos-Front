1312e7543bbf0bef0d5040654dfebd15602d3bfe
Kauanne Cavalcante <kca1@discente.ifpe.edu.br>
2023-12-05 22:46:15 -0300

    Fix: teste respostas do egresso

--- src/components/formEgresso/allFormFields.jsx
+++ src/components/formEgresso/allFormFields.jsx
@@ -7,6 +7,7 @@ import noFields from "/public/icons/noFields.svg";
 export default function AllFields({ formType, hasFields, setHasFields }) {
     const [fields, setFields] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [loader, setLoader] = useState(true);
 
     const backHome = () => {
         window.location.href = "/egresso/home";
@@ -16,123 +17,122 @@ export default function AllFields({ formType, hasFields, setHasFields }) {
         if (hasFields) {
             getAllFormFields(formType)
                 .then((response) => {
-                    setFields(response || []);
-                    setLoading(false);
+                    console.log(response)
+                    setFields(response);
+                    setLoader(false);
                 })
-                .catch((error) => {
-                    console.error(error);
-                    setFields([]);
-                    setLoading(false);
-                });
         }
     }, [hasFields, formType]);
 
-    const renderLoading = () => {
+    if (fields.statusCode !== 404) {
         return (
-            <div className="flex flex-col items-center justify-center">
-                {/* spin */}
-                <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-azulBase"></div>
-                <p className="text-legenda text-azulBase mt-2">Carregando...</p>
+            <div>
+                {loader ? (
+                    <div className="flex flex-col items-center justify-center my-50">
+                        <div class="spinner" />
+                    </div>
+                ) : (
+                    fields.map((field, index) => (
+                        <div>
+                            <div className="flex-grow">
+                                <div className="bg-fundo mt-15 rounded-10 py-30 px-60">
+                                    <div className="flex gap-10 items-center">
+                                        <label className="text-subtitulo text-pretoTexto font-semibold">{field.question}</label>
+                                    </div>
+                                    {field.type === 'text' && (
+                                        <input
+                                            type="text"
+                                            placeholder="Digite aqui"
+                                            id="nome"
+                                            className="bg-fundo w-full border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo pl-10 font-regular mt-15"
+                                        />
+                                    )}
+                                    {field.type === 'radio' && (
+                                        <div>
+                                            {field.options.map((option, index) => (
+                                                <div className="flex gap-10 mt-15">
+                                                    <div className="flex w-full">
+                                                        <input type="radio" name="radioOptions" />
+                                                        <label className="w-full bg-inherit outline-none text-pretoTexto text-paragrafo font-regular ml-10">{option.question}</label>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                            {field.other === 'Outros:' && (
+                                                <div className="flex gap-10 mt-15">
+                                                    <div className="flex w-full">
+                                                        <input
+                                                            type="radio"
+                                                            name="radioOptions"
+                                                            value="other"
+                                                            className="mr-10"
+                                                        />
+                                                        <label>Outros:</label>
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Digite aqui"
+                                                            className="w-full bg-inherit border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo font-regular ml-10"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
+                                    )}
+                                    {field.type === 'checkbox' && (
+                                        <div>
+                                            {field.options.map((option, index) => (
+                                                <div className="flex gap-10 mt-15">
+                                                    <div className="flex w-full">
+                                                        <input type="checkbox" name="checkboxOptions" />
+                                                        <input
+                                                            type="text"
+                                                            className="w-full bg-inherit outline-none text-pretoTexto text-paragrafo font-regular ml-10"
+                                                            />
+                                                    </div>
+                                                </div>
+                                            ))}
+                                            {field.other === 'Outros:' && (
+                                                <div className="flex gap-10 mt-15">
+                                                    <div className="flex w-full">
+                                                        <input
+                                                            type="checkbox"
+                                                            name="radioOptions"
+                                                            value="other"
+                                                            className="mr-10"
+                                                        />
+                                                        <label>Outros:</label>
+                                                        <input
+                                                            type="text"
+                                                            placeholder="Digite aqui"
+                                                            className="w-full bg-inherit border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo font-regular ml-10"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
-        );
-    };
-
-    const renderField = (field) => {
-        if (field.type === "text") {
-            return (
-                <div className="flex flex-col space-y-5">
-                    <p className="text-pretoTexto text-subtitulo font-semibold">{field.question}</p>
-                    <input
-                        className="w-full bg-fundo px-10 py-10 text-pretoTexto border-b-2 border-cinza07 focus:outline-none text-input required"
-                        type="text"
-                        placeholder="Digite aqui"
-                    />
-                </div>
-            );
-        } else if (field.type === "radio") {
-            return (
-                <div className="flex flex-col space-y-5">
-                    <p className="text-pretoTexto text-subtitulo font-semibold">{field.question}</p>
-                    {field.options.map((option, index) => (
-                        <div className="flex gap-10 mt-15" key={index}>
-                            <div className="flex w-full">
-                                <input type="radio" name="radioOptions" value={index} disabled />
-                                <input
-                                    type="text"
-                                    value={option.question}
-                                    className="w-full bg-inherit border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo font-regular ml-10"
-                                />
-                            </div>
+        )
+    } else {
+        return (
+            <div>
+                {loader ? (
+                    <div className="flex flex-col items-center justify-center my-50">
+                        <div class="spinner" />
+                    </div>
+                ) : (    
+                    hasFields && (
+                        <div className="flex flex-col items-center justify-center">
+                            <Image className="mt-90" src={noFields} alt="Ícone representando que não tem campos no formulário" />
+                            <p className="text-tituloSessão text-cinza05 text-center mt-15 mb-90">Ainda não há campos no formulário. <br></br> Comece agora!</p>
                         </div>
-                    ))}
-                    {field.other === "Outros:" && (
-                        <div className="flex gap-10 mt-15">
-                            <div className="flex w-full">
-                                <input
-                                    type="radio"
-                                    name="radioOptions"
-                                    value="other"
-                                    className="mr-10"
-                                    disabled
-                                />
-                                <label>Outros:</label>
-                                <input
-                                    type="text"
-                                    placeholder="Digite aqui"
-                                    disabled
-                                    className="w-full bg-inherit border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo font-regular ml-10"
-                                />
-                            </div>
-                        </div>
-                    )}
-                </div>
-            );
-        } else if (field.type === "checkbox") {
-            return (
-                <div>
-                    {field.options.map((option, index) => (
-                        <div className="flex gap-10 mt-15" key={index}>
-                            <div className="flex w-full">
-                                <input type="checkbox" name="checkboxOptions" value={index} disabled />
-                                <input
-                                    type="text"
-                                    value={option.question}
-                                    className="w-full bg-inherit border-b-1 border-cinza07 outline-none text-pretoTexto text-paragrafo font-regular ml-10"
-                                />
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            );
-        }
-    };
-
-    const renderFields = () => {
-        if (loading) {
-            return renderLoading(); // Se estiver carregando, exiba o componente de carregamento
-        } else if (fields.statusCode === 404) {
-            return (
-                <div className="flex flex-col items-center justify-center">
-                    <Image
-                        className="mt-90"
-                        src={noFields}
-                        alt="Ícone representando que não tem campos no formulário"
-                    />
-                    <p className="text-tituloSessão text-cinza05 text-center mt-15 mb-90">
-                        Ainda não há campos nesta seção
-                    </p>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    {fields.map((field, index) => (
-                        <div key={index}>{renderField(field)}</div>
-                    ))}
-                </div>
-            );
-        }
-    };
-
-    return <div>{renderFields()}</div>;
+                    )
+                )}
+            </div>
+        )
+    }
 }
